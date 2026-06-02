d43473f4c20a83fa135f330f4df64981b3577012
Egor Chumichev <egor@chumichev.dev>
2026-06-02 16:22:09 +0500

    chore: fallback to Let's Encrypt production server

--- resources/manager.sh
+++ resources/manager.sh
@@ -4,7 +4,7 @@ set -eu
 
 while true; do
 	echo "[lego] Starting DNS-01 Challenge..."
-	./lego run --path /var/lib/lego/ --server letsencrypt-staging --dns yandexcloud --email $DOMAIN_EMAIL --domains $DOMAIN_NAME --dns.resolvers 8.8.8.8:53 --accept-tos
+	./lego run --path /var/lib/lego/ --dns yandexcloud --email $DOMAIN_EMAIL --domains $DOMAIN_NAME --dns.resolvers 8.8.8.8:53 --accept-tos
 
   echo "[lego] Sleeping 24h..."
   sleep 20s
