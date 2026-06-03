83ac52a2efd34f9430ae1c65c22ae8addf77b99b
Egor Chumichev <egor@chumichev.dev>
2026-06-03 15:21:55 +0500

    feat: add ACME server variable

--- resources/manager.sh
+++ resources/manager.sh
@@ -4,7 +4,7 @@ set -eu
 
 while true; do
 	echo "[lego] Starting DNS-01 Challenge..."
-	./lego run --path /var/lib/lego/ --dns yandexcloud --email $DOMAIN_EMAIL --domains $DOMAIN_NAME --dns.resolvers 8.8.8.8:53 --accept-tos
+	./lego run --path /var/lib/lego/ --server $ACME_PROVIDER_NAME --dns yandexcloud --email $DOMAIN_EMAIL --domains $DOMAIN_NAME --dns.resolvers 8.8.8.8:53 --accept-tos
 
   echo "[lego] Sleeping 24h..."
   sleep 24h
