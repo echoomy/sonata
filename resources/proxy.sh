5f1dab1a66b5ed26f96e4656ec75dd8f2246f0f6
Egor Chumichev <egor@chumichev.dev>
2026-06-03 16:15:20 +0500

    feat: use XHTTP to hide the traffic

--- resources/proxy.sh
+++ resources/proxy.sh
@@ -6,8 +6,9 @@ set -eu
 : "${PRIVATE_KEY:?PRIVATE_KEY is required}"
 : "${CLIENT_SHORT_ID:?CLIENT_SHORT_ID is required}"
 : "${DOMAIN_NAME:?DOMAIN_NAME is required}"
+: "${CLIENT_XHTTP_PATH:?CLIENT_XHTTP_PATH is required}"
 
-envsubst '${CLIENT_UUID} ${PRIVATE_KEY} ${CLIENT_SHORT_ID} ${DOMAIN_NAME}' \
+envsubst '${CLIENT_UUID} ${PRIVATE_KEY} ${CLIENT_SHORT_ID} ${DOMAIN_NAME} ${CLIENT_XHTTP_PATH}' \
 	< /etc/xray/config.template.json > /etc/xray/config.json
 
 exec xray --config /etc/xray/config.json
